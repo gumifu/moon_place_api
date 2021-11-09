d4f30adb401a4662b3acacd460f16cf98104f8d7
gumifu <ffumi.66@gmail.com>
2021-11-09 13:49:17 +0900

    first

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+}
+function draw() {
+    background(0, 30);
+    ellipse(mouseX, mouseY, 40, 40);
+    fill(230);
+    noStroke();
+}
